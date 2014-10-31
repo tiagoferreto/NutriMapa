4f9dfaab7cd1db4adf9fd1673ae8e225d6997e60
pipez <felipe_angnes@hotmail.com>
2014-10-31 15:28:55 -0200

    PHPs funcionando (loacais e local)

--- locais.php
+++ locais.php
@@ -11,7 +11,7 @@
 
 <body>
 	<header>
-		<a href="index.html"><img  style="margin-top:20px;margin-left:30px" src="icones/claros/Logo.png"></a>
+		<a href="index.html"><img  style="margin-top:20px;margin-left:30px;widht:130px;height:130px" src="icones/claros/Logo.png"></a>
 		<a href="sobre.html"><img align="right" style="margin-top:40px;margin-right:50px" src="icones/claros/Sobre.png"></a>
 		<a href="favoritos.html"><img align="right" style="margin-top:37px;margin-right:40px" src="icones/claros/Favoritos.png"></a>
 		<a href="locais.html"><img align="right" style="margin-top:34px;margin-right:40px" src="icones/escuros/Locais2.png"></a>
@@ -23,9 +23,6 @@
 	<div id= "caixa_lojas">
 		<h3> Lojas</h3>
 	<div class ="divs">
-		<?php echo "Teste"
-$db = new SQLite3('nutrimapa.sqlite') or echo 'Unable to open database';
-?>
 		<?php
 			$top = TRUE;
 			$pos = "top";
--- local.php
+++ local.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 	<header>
-		<a href="index.html"><img  style="margin-top:20px;margin-left:30px" src="icones/claros/Logo.png"></a>
+		<a href="index.html"><img  style="margin-top:20px;margin-left:30px;widht:130px;height:130px" src="icones/claros/Logo.png"></a>
 		<a href="sobre.html"><img align="right" style="margin-top:40px;margin-right:50px" src="icones/claros/Sobre.png"></a>
 		<a href="favoritos.html"><img align="right" style="margin-top:37px;margin-right:40px" src="icones/claros/Favoritos.png"></a>
 		<a href="locais.html"><img align="right" style="margin-top:34px;margin-right:40px" src="icones/escuros/Locais2.png"></a>
