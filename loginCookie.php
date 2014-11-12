a2da73d34594f10fb7a0d87e2df78ce17f3da5ae
João Otávio Fanti de Souza <joao.otavio@acad.pucrs.br>
2014-11-12 13:34:39 -0200

    Cookies

--- loginCookie.php
+++ loginCookie.php
@@ -6,25 +6,23 @@ echo 'REDIRECIONANDO';
 echo nl2br("\n");
 $DB = new SQLite3('nutrimapa.sqlite') or die ('Unable to open DB');
 $selectQuery = $DB -> query('SELECT * FROM usuarios');
-
 while($row = $selectQuery -> fetchArray() ){
     $valor = $row ['id'];
-
     if($ver == $row['user_name']){
-	   $value = $valor;
+       $value = $valor;
     }
     
 }
 if(!isset($_COOKIE['cookieNome'])) {
-	$cookieNome = 'usuarioCookie';
+    $cookieNome = 'usuarioCookie';
     setcookie('cookieNome', $value, time() + 3600, "/");
     echo "condicao 1";
     echo nl2br("\n");
     header("Location: http://192.168.10.10/redireciona.php");
 } else {
-	$cookieNome = 'usuarioCookie';
+    $cookieNome = 'usuarioCookie';
     unset($_COOKIE['cookieNome']);
-	setcookie('cookieNome', '', time() - 3600, "/");
+    setcookie('cookieNome', '', time() - 3600, "/");
     setcookie('cookieNome', $value, time() + 3600, "/");
     echo "condicao 2";
     header("Location: http://192.168.10.10/redireciona.php");
