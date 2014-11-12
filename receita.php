e657633f1ed76277f32de00050e17f7169da5343
João Otávio Fanti de Souza <joao.otavio@acad.pucrs.br>
2014-11-12 16:17:24 -0200

    COOKIES

--- receita.php
+++ receita.php
@@ -12,7 +12,7 @@
 	<header>
 		<a href="mapas.php"><img  style="margin-top:20px;margin-left:30px;widht:130px;height:130px" src="icones/logo.png"></a>
 		<a href="sobre.html"><img align="right" style="margin-top:40px;margin-right:50px" src="icones/sobre_escuro.png"></a>
-		<a href="favoritos.html"><img align="right" style="margin-top:37px;margin-right:40px" src="icones/favoritos_escuro.png"></a>
+		<a href="favoritos.php"><img align="right" style="margin-top:37px;margin-right:40px" src="icones/favoritos_escuro.png"></a>
 		<a href="locais.php"><img align="right" style="margin-top:34px;margin-right:40px" src="icones/locais_escuro.png"></a>
 		<a href="receitas.php"><img align="right" style="margin-top:38px;margin-right:40px" src="icones/receitas_claro.png"></a>
 		<a href="mapas.php"><img align="right" style="margin-top:42px;margin-right:40px" src="icones/mapa_escuro.png"></a>
