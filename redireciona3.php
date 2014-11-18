356464509194cf5fbed610c062bb80db66eaf670
João Otávio Fanti de Souza <joaofantisouza@hotmail.com>
2014-11-18 15:57:21 -0200

    Delete redireciona3.php

--- redireciona3.php
+++
@@ -1,6 +0,0 @@
-<?php
-$var3 = $_GET['id'];
-$db = new SQLite3('nutrimapa.sqlite') or die ('Unable to open DB');
-$db ->query('DELETE FROM favoritos WHERE lid='.$var3);
-header("Location: http://192.168.10.10/local.php?id=".$var3);
-?>
