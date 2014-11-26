5ede6181366d2e3a12d2e5d491d53d31e64c8d94
gitVinicius <vinicius.blembeel@acad.pucrs.br>
2014-11-26 16:19:02 -0200

    Comentarios 99 por cento

--- redireciona2.php
+++ redireciona2.php
@@ -6,6 +6,16 @@ $date=date_create(NULL, timezone_open("America/Sao_Paulo"));
 $dateP = "'".date_format($date,"d/m/Y H:i")."'";
 echo $dateP;
 $db = new SQLite3('nutrimapa.sqlite') or die('Unable to open database');
-$db -> query('INSERT INTO comentarios (uid, lid, coment, datahora) VALUES ('.$uid.','.$var.','.$com.','.$dateP.')');
+$tcomentarios2 = $db ->query('SELECT * FROM comentarios WHERE lid ='.$var);
+
+$x=0;
+while($row10 = $tcomentarios2 -> fetchArray())
+{
+	$x=$x+1;
+}
+
+$x=$x+1;
+
+$db -> query('INSERT INTO comentarios (uid, lid, coment, datahora, numerocom) VALUES ('.$uid.','.$var.','.$com.','.$dateP.','.$x.')');
 header ('Location: http://192.168.10.10/local.php?id='.$var);
 ?>
